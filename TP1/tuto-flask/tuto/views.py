30530d7ef1f11e0d0294ae940ca7612a303a6b85
Yannis-barache <ybarache@icloud.com>
2023-09-12 15:34:31 +0200

    Ajout des livres

--- TP1/tuto-flask/tuto/views.py
+++ TP1/tuto-flask/tuto/views.py
@@ -24,7 +24,7 @@ def page2():
 @app.route("/books")
 def books():
     return render_template(
-        "books.html",
+        "livre.html",
         title="Books",
         books=get_sample()
     )
